9ea4cfdec3edbcae0183e8eec89573cc554990bf
Chuckz Okoye <tricelex@gmail.com>
2020-07-11 15:56:13 +0100

    Created course models

---
+++ courses/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+from courses.models import Module, Subject, Course
+
+
+# Register your models here.
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class ModuleInline(admin.StackedInline):
+    model = Module
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'subject', 'created']
+    list_filter = ['created', 'subject']
+    search_fields = ['title', 'overview']
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [ModuleInline]
